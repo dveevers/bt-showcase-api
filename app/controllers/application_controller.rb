b4377e5b2923032cfc7339f4c3e7e725cf2363b3
Philippe Stemberger <pstember@gmail.com>
2018-04-21 13:03:44 +0100

    dummy reporting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,11 @@ class ApplicationController < ActionController::API
     #conn = TCPSocket.new 'carbon.hostedgraphite.com', 2003
     #conn.puts apikey + ".#{category}.#{item} #{content}\n"
     #conn.close
+    puts "804e6ce0-6659-431e-acf0-b75f6e650fa8.test.testing 1.2\n"
     conn = TCPSocket.new '0168403f.carbon.hostedgraphite.com', 2003
     conn.puts "804e6ce0-6659-431e-acf0-b75f6e650fa8.test.testing 1.2\n"
     conn.close
-    puts apikey + ".#{category}.#{item} #{content}"
+    #puts apikey + ".#{category}.#{item} #{content}"
   end
 
 end
