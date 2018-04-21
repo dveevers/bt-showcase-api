2df179c5c1845a5bc8b091d1869a23ad2b444059
Philippe Stemberger <pstember@gmail.com>
2018-04-21 13:58:35 +0100

    dummy reporting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::API
 
   def createLog content, item, category='report'
     apikey = ENV['HOSTEDGRAPHITE_APIKEY']
-    conn = TCPSocket.new 'carbon.hostedgraphite.com', 2003
+    conn = TCPSocket.new '4d70dd2a.carbon.hostedgraphite.com', 2003
     conn.puts apikey + ".#{category}.#{item} #{content}\n"
     conn.close
     puts apikey + ".#{category}.#{item} #{content}"
