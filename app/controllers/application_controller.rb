41e0cc823ada4c27adc70488ea14bef52640e6b7
Philippe Stemberger <pstember@gmail.com>
2018-04-21 13:01:43 +0100

    dummy reporting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,12 @@ class ApplicationController < ActionController::API
   include ExceptionHandler
 
   def createLog content, item, category='report'
-    apikey = ENV['HOSTEDGRAPHITE_APIKEY']
-    conn = TCPSocket.new 'carbon.hostedgraphite.com', 2003
-    conn.puts apikey + ".#{category}.#{item} #{content}\n"
+    #apikey = ENV['HOSTEDGRAPHITE_APIKEY']
+    #conn = TCPSocket.new 'carbon.hostedgraphite.com', 2003
+    #conn.puts apikey + ".#{category}.#{item} #{content}\n"
+    #conn.close
+    conn = TCPSocket.new '0168403f.carbon.hostedgraphite.com', 2003
+    conn.puts "804e6ce0-6659-431e-acf0-b75f6e650fa8.test.testing 1.2\n"
     conn.close
     puts apikey + ".#{category}.#{item} #{content}"
   end
